ee2a1db1645f8da71a171f524bbbd872d99c9701
James Wachmann <james.wachmann@gmail.com>
2021-06-13 23:57:05 -0400

    Remove prompt when launching difftool or mergetool

--- setup_p4merge.sh
+++ setup_p4merge.sh
@@ -9,8 +9,10 @@ git config --global mergetool.p4mergetool.cmd \
 "/Applications/p4merge.app/Contents/Resources/launchp4merge \$PWD/\$BASE \$PWD/\$REMOTE \$PWD/\$LOCAL \$PWD/\$MERGED"
 git config --global mergetool.p4mergetool.trustExitCode false
 git config --global mergetool.keepBackup false
+git config --global mergetool.prompt false
 
 # Setup p4merge as a visual diff tool
 git config --global diff.tool p4mergetool
 git config --global difftool.p4mergetool.cmd \
 "/Applications/p4merge.app/Contents/Resources/launchp4merge \$LOCAL \$REMOTE"
+git config --global difftool.prompt false
