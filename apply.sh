654d0c9da3fc323fb35b562f620661a891a1b37e
James Wachmann <james.wachmann@gmail.com>
2021-07-22 16:26:28 -0400

    Update packages to install

--- apply.sh
+++ apply.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+# Install packages
+brew install git
+brew install fzf
+brew install ripgrep
+brew install vim
+brew install thefuck
+brew install shellcheck
+brew install ncdu
+brew install fd
+brew install broot
+brew install watch
+brew install tmux
+brew install tldr
+brew install curl
+brew install htop
+brew install nvm
+brew install tfenv
+brew install k9s
+brew install go
+brew install tokei
+brew install owasp-zap
+brew install wireshark
+brew install postman
+brew install lolcat
+brew install cowsay
+brew install fortune
+brew install asciiquaruim
+brew install cmatrix
+brew install figlet
+
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -9,16 +39,13 @@ cp -i zshrc ~/.zshrc
 # Apply vim preferences
 cp -i vimrc ~/.vimrc
 
-# Install plugins
+# Install ZSH plugins
 git clone https://github.com/MichaelAquilina/zsh-auto-notify.git $ZSH_CUSTOM/plugins/auto-notify
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 ~/.scm_breeze/install.sh
-
-brew install fzf
-brew install ripgrep
 
 # Install terminal theme (Powerlevel10k)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
